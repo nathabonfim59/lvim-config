afa3c4f6351a16137c457c23ddd5eccb62184872
Nathanael <dev@nathabonfim59.com>
2023-05-07 13:42:50 -0300

    feat(ftplugin/dart): support for the dartls

---
+++ plugin/ftplugin/dart.lua
@@ -0,0 +1,14 @@
+-- Set up LSP for Dart
+if vim.fn.executable("dart") == 1 then
+	require("lspconfig").dartls.setup({
+	})
+
+	-- Define a custom mapping for the LSP if the dart sdk is available
+
+	-- Open docs with Shift + K
+	vim.cmd[[nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>]]
+
+	-- Go to definition with gd
+	vim.cmd[[nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>]]
+end
+
