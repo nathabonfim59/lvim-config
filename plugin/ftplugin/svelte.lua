8bb9c91a959e8d6f9e88cf4dc874b92703fcac38
nathabonfim59 <dev@nathabonfim59.com>
2023-08-02 17:09:41 -0300

    feat(plugins): support for tailwindcss in svelte

---
+++ plugin/ftplugin/svelte.lua
@@ -0,0 +1,4 @@
+require("lvim.lsp.manager").setup("svelte", {
+    filetypes = {"svelte"}
+})
+require("lvim.lsp.manager").setup("tailwindcss")
--- plugin/ftplugin/tpl.lua
+++ plugin/ftplugin/tpl.lua
@@ -5,3 +5,5 @@ augroup FiletypeTPL
   autocmd BufEnter *.tpl lua vim.api.nvim_buf_set_option(0, 'syntax', 'php')
 augroup END
 ]])
+
+require("lvim.lsp.manager").setup("html")
