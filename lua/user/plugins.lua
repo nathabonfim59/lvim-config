8c3afcb4c2b002957ceb1b3fa5572cb6596256ce
nathabonfim59 <dev@nathabonfim59.com>
2023-12-08 15:24:59 -0300

    feat(plugins/flatten.nvim): open file in the same nvim instance

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -141,6 +141,20 @@ lvim.plugins = {
 	{
 		"lvimuser/lsp-inlayhints.nvim",
 	},
+	-- Add support for opening in the same instance
+	{
+		"willothy/flatten.nvim",
+		-- config = true,
+		-- or pass configuration with
+		opts = {
+			window = {
+				open = "alternate"
+			}
+		},
+		-- Ensure that it runs first to minimize delay when opening file from terminal
+		lazy = false,
+		priority = 1001,
+	},
 }
 
 lvim.transparent_window = true
