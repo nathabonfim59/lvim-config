3e7ab4aa3c91051ef4555fbd5235df8587fae71e
nathabonfim59 <dev@nathabonfim59.com>
2023-12-08 15:25:42 -0300

    feat(ftplugin): add tailwindcss to Vue and PHP

--- plugin/ftplugin/php.lua
+++ plugin/ftplugin/php.lua
@@ -3,4 +3,4 @@ vim.cmd[[setlocal tabstop=4]]
 
 -- Disable rarely used LSPs
 -- require("lvim.lsp.manager").setup("phpactor")
--- require("lvim.lsp.manager").setup("tailwindcss")
+require("lvim.lsp.manager").setup("tailwindcss")
